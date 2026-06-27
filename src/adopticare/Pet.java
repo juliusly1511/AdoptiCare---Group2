5b6cac32a2aca813eda4140573f07e53b91dde6a
Lee-stud <lunalee694@gmail.com>
2026-06-27 22:30:52 +0800

    Update Pet.java with Comments

--- src/adopticare/Pet.java
+++ src/adopticare/Pet.java
@@ -2,6 +2,7 @@ package adopticare;
 
 public class Pet {
 
+    // ===== Private Fields/Attributes of Pet Class
     private int petId;
     private String petName;
     private String species;
@@ -13,7 +14,8 @@ public class Pet {
     private String nextVaccinationSchedule;
     private String adoptionStatus;
     private boolean archived;
-
+    
+    //Pet Constructor parameterized/non-parameterized
     public Pet() {
     }
 
@@ -37,4 +39,95 @@ public class Pet {
         this.archived = archived;
         
     }
+    
+    //Getter and Setter Method of Private Fields
+    public int getPetId() {
+        return petId;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHealthCondition() {
+        return healthCondition;
+    }
+
+    public void setHealthCondition(String healthCondition) {
+        this.healthCondition = healthCondition;
+    }
+
+    public String getVaccinationStatus() {
+        return vaccinationStatus;
+    }
+
+    public void setVaccinationStatus(String vaccinationStatus) {
+        this.vaccinationStatus = vaccinationStatus;
+    }
+
+    public String getLastVaccinationDate() {
+        return lastVaccinationDate;
+    }
+
+    public void setLastVaccinationDate(String lastVaccinationDate) {
+        this.lastVaccinationDate = lastVaccinationDate;
+    }
+
+    public String getNextVaccinationSchedule() {
+        return nextVaccinationSchedule;
+    }
+
+    public void setNextVaccinationSchedule(String nextVaccinationSchedule) {
+        this.nextVaccinationSchedule = nextVaccinationSchedule;
+    }
+
+    public String getAdoptionStatus() {
+        return adoptionStatus;
+    }
+
+    public void setAdoptionStatus(String adoptionStatus) {
+        this.adoptionStatus = adoptionStatus;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
+
+    
 }
