64bbb7c51217bb763424e1d8171dad372c6f4723
Lee-stud <lunalee694@gmail.com>
2026-06-27 21:13:39 +0800

    Initial Commit

---
+++ src/adopticare/Database.java
@@ -0,0 +1,21 @@
+package adopticare;
+
+import java.sql.*;
+
+public class Database {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/adopticaredb";
+    
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection()
+            throws SQLException {
+        
+        return DriverManager.getConnection(
+                URL, 
+                USER, 
+                PASSWORD
+        );
+    }
+}
